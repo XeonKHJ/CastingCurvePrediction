9a23a5d631c3d01cf311055e90d8519d22908a47
康宏嘉 <redalertkhj@live.cn>
2021-11-08 23:00:31 +0800

    建立塞棒

--- src/system/frontend/src/animation.js
+++ src/system/frontend/src/animation.js
@@ -1,22 +1,115 @@
-var VSHADER_SOURCE =
-    'attribute vec4 a_Position;\n' +
-    'void main() {\n' +
-    '  gl_Position = a_Position;\n' +
-    '}\n';
-
-// Fragment shader program
-var FSHADER_SOURCE =
-    'void main() {\n' +
-    '  gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);\n' +
-    '}\n';
-
-function main() {
+function startAnimation() {
+    // Init webgl context.
     var canvas = document.getElementById('webgl');
-
     var gl = getWebGLContext(canvas);
-    if (!gl) {
-        console.log('Failed to get the rendering context.');
+
+    startDrawing(gl);
+}
+
+function startDrawing(gl) {
+    gl.clearColor(0, 0, 0, 0.5);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    drawTudish(gl);
+    drawSTP(gl);
+}
+
+
+function drawSTP(gl) {
+    var VSHADER_SOURCE =
+        'attribute vec4 a_Position;\n' +
+    'void main() {\n' +
+        '  gl_Position = a_Position;\n' +
+        '}\n';
+
+    // Fragment shader program
+    var FSHADER_SOURCE =
+        'void main() {\n' +
+        '  gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);\n' +
+        '}\n';
+
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to intialize shaders.');
         return;
+    }
+
+    var n = initSTPVertexBuffers(gl);
+    if (n < 0) {
+        console.log('Failed to set the positions of the vertices');
+        return;
+    }
+
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
+}
+
+function initSTPVertexBuffers(gl) {
+    var vertices = new Float32Array([
+        -50 / width, 500 / height,
+        50 / width, 500 / height,
+        -50 / width, 50 / height,
+        50 / width, 50 / height,
+        0, 0
+    ]);
+
+    var n = vertices.length / 2;
+
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failed to create the buffer object');
+        return -1;
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Filed to get the strorage location of a_Position')
+        return -1;
+    }
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position)
+
+    return n;
+}
+
+var height = 2000;
+var width = 1000;
+function drawTudish(gl) {
+    var VSHADER_SOURCE =
+        'attribute vec4 a_Position;\n' +
+        'void main() {\n' +
+        '  gl_Position = a_Position;\n' +
+        '}\n';
+
+    // Fragment shader program
+    var FSHADER_SOURCE =
+        'void main() {\n' +
+        '  gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);\n' +
+        '}\n';
+
+    var tan = Math.tan(80 / 180 * Math.PI);
+    var tudishLeftVertices = new Float32Array(
+        [
+            -400 / width, (50 * tan) / height,
+            -350 / width, (50 * tan) / height,
+            -350 / width, 0,
+            (-350 + ((50 * tan - 50) / tan)) / width, 50 / height,
+            -50 / width, 0,
+            -50 / width, 50 / height
+        ]
+    );
+
+    var tudishRightVertices = new Float32Array(
+        12
+    );
+
+    for (var i = 0; i < tudishLeftVertices.length; i++) {
+        if (i % 2) {
+            tudishRightVertices[i] = tudishLeftVertices[i];
+        }
+        else {
+            tudishRightVertices[i] = -1 * tudishLeftVertices[i];
+        }
     }
 
     if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
@@ -24,17 +117,14 @@ function main() {
         return;
     }
 
-    var n = initVertexBuffersNew(gl);
+    var n = initTudishVertexBuffers(gl, tudishLeftVertices);
     if (n < 0) {
         console.log('Failed to set the positions of the vertices');
         return;
     }
-
-    gl.clearColor(0, 0, 0, 0.5);
-    gl.clear(gl.COLOR_BUFFER_BIT);
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
 
-    n = initVertexBuffersNew2(gl);
+    n = initTudishVertexBuffers(gl, tudishRightVertices);
     if (n < 0) {
         console.log('Failed to set the positions of the vertices');
         return;
@@ -42,76 +132,8 @@ function main() {
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
 }
 
-function initVertexBuffers(gl) {
-    var vertices = new Float32Array([
-      -0.5, 0.5,   -0.5, -0.5,   0.5, 0.5,　0.5, -0.5
-    ]);
-    var n = 4; // The number of vertices
-  
-    // Create a buffer object
-    var vertexBuffer = gl.createBuffer();
-    if (!vertexBuffer) {
-      console.log('Failed to create the buffer object');
-      return -1;
-    }
-  
-    // Bind the buffer object to target
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    // Write date into the buffer object
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-  
-    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-    if (a_Position < 0) {
-      console.log('Failed to get the storage location of a_Position');
-      return -1;
-    }
-  　// Assign the buffer object to a_Position variable
-    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
-  
-    // Enable the assignment to a_Position variable
-    gl.enableVertexAttribArray(a_Position);
-  
-    return n;
-  }
-
-function initVertexBuffersNew(gl) {
-    var height = 2000;
-    var width = 1000;
-    var tudishAngle = 80;
-    var tan = Math.tan(80/180 * Math.PI);
-    var tudishLeftVertices = new Float32Array(
-        [
-            -400/width, (50 * tan)/height, 
-            -350/width, (50 * tan)/height, 
-            -350/width, 0,
-            (-350 + ((50*tan - 50) / tan)) /width, 50/height,
-            -50/width, 0,
-            -50/width, 50/height
-        ]
-    );
-    var tudishRightVertices = new Float32Array(
-        12
-    );
-
-    for(var i = 0; i < tudishLeftVertices.length; i++)
-    {
-        if(i % 2)
-        {
-            tudishRightVertices[i] = tudishLeftVertices[i];
-        }
-        else
-        {
-            tudishRightVertices[i] = -1 * tudishLeftVertices[i];
-        }
-    }
-
-
-    // var tudishVertices = new Float32Array([
-    //     -0.5, 0.5, -0.5, -0.5, 0.5, 0.5, 0.5, -0.5
-    // ]);
-
-    var n = tudishLeftVertices.length / 2;
-    var stride = tudishLeftVertices.BYTES_PER_ELEMENT;
+function initTudishVertexBuffers(gl, vertices) {
+    var n = vertices.length / 2;
 
     var vertexBuffer = gl.createBuffer();
     if (!vertexBuffer) {
@@ -120,7 +142,7 @@ function initVertexBuffersNew(gl) {
     }
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, tudishLeftVertices, gl.STATIC_DRAW)
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
 
     var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
     if (a_Position < 0) {
@@ -129,79 +151,6 @@ function initVertexBuffersNew(gl) {
     }
     gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(a_Position)
-    
+
     return n;
-}
-
-function initVertexBuffersNew2(gl) {
-    var height = 2000;
-    var width = 1000;
-    var tudishAngle = 80;
-    var tan = Math.tan(80/180 * Math.PI);
-    var tudishLeftVertices = new Float32Array(
-        [
-            -400/width, (50 * tan)/height, 
-            -350/width, (50 * tan)/height, 
-            -350/width, 0,
-            (-350 + ((50*tan - 50) / tan)) /width, 50/height,
-            -50/width, 0,
-            -50/width, 50/height
-        ]
-    );
-    var tudishRightVertices = new Float32Array(
-        12
-    );
-
-    for(var i = 0; i < tudishLeftVertices.length; i++)
-    {
-        if(i % 2)
-        {
-            tudishRightVertices[i] = tudishLeftVertices[i];
-        }
-        else
-        {
-            tudishRightVertices[i] = -1 * tudishLeftVertices[i];
-        }
-    }
-
-
-    // var tudishVertices = new Float32Array([
-    //     -0.5, 0.5, -0.5, -0.5, 0.5, 0.5, 0.5, -0.5
-    // ]);
-
-    var n = tudishLeftVertices.length / 2;
-    var stride = tudishLeftVertices.BYTES_PER_ELEMENT;
-
-    var vertexBuffer = gl.createBuffer();
-    if (!vertexBuffer) {
-        console.log('Failed to create the buffer object');
-        return -1;
-    }
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, tudishRightVertices, gl.STATIC_DRAW)
-
-    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-    if (a_Position < 0) {
-        console.log('Filed to get the strorage location of a_Position')
-        return -1;
-    }
-    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(a_Position)
-    
-    return n;
-}
-
-function readFileAsString(path) {
-    var files = this.files;
-    if (files.length === 0) {
-        console.log('No file is selected');
-        return;
-    }
-
-    var reader = new FileReader();
-    reader.onload = function (event) {
-        console.log('File content:', event.target.result);
-    };
-    reader.readAsText(files[0]);
 }
