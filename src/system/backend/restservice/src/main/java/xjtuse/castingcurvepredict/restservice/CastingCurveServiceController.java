67adc0d761b8122283156459bbc253ff29fcda59
康宏嘉 <redalertkhj@live.cn>
2021-11-30 16:03:05 +0800

    implement "get trainning status" REST API.

--- src/system/backend/restservice/src/main/java/xjtuse/castingcurvepredict/restservice/CastingCurveServiceController.java
+++ src/system/backend/restservice/src/main/java/xjtuse/castingcurvepredict/restservice/CastingCurveServiceController.java
@@ -2,10 +2,14 @@ package xjtuse.castingcurvepredict.restservice;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.UUID;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,6 +18,7 @@ import xjtuse.castingcurvepredict.interfaces.ICastingGenerator;
 import xjtuse.castingcurvepredict.models.CastingModel;
 import xjtuse.castingcurvepredict.models.CastingResultModel;
 import xjtuse.castingcurvepredict.viewmodels.DiagramViewModel;
+import xjtuse.castingcurvepredict.viewmodels.TrainningStatusViewModel;
 import xjtuse.castingcurvepredict.viewmodels.UploadResultViewModel;
 
 @CrossOrigin
@@ -24,7 +29,6 @@ public class CastingCurveServiceController {
         // Construct a diagram view model;
         ICastingGenerator generator = new ConstCastingGenerator();
 
-        // Stab
         CastingModel castingModel = new CastingModel(generator);
 
         CastingResultModel resultModel = castingModel.PredictCastingCurve(null);
@@ -32,6 +36,8 @@ public class CastingCurveServiceController {
         return new DiagramViewModel(resultModel);
     }
 
+
+    HashMap<UUID, TrainningStatusViewModel> uploadList = new HashMap<UUID, TrainningStatusViewModel>();
     @PostMapping("/uploadAndTrainModel")
     public UploadResultViewModel uploadAndTrainModel(MultipartFile file)
     {
@@ -40,9 +46,17 @@ public class CastingCurveServiceController {
             resultViewModel.setStatus("文件为空。");
         }
 
+        
         String fileName = file.getOriginalFilename();
         String filePath = CastingConfig.getModelFolderPath();
         File dest = new File(filePath + fileName);
+
+        // check file.
+        TrainningStatusViewModel statusViewModel = new TrainningStatusViewModel();
+        // Generate UUID.
+        UUID uuid = UUID.randomUUID();
+        uploadList.put(uuid, statusViewModel);
+
         try {
             file.transferTo(dest);
             resultViewModel.setStatus("上传成功");
@@ -50,19 +64,30 @@ public class CastingCurveServiceController {
             resultViewModel.setStatus("上传失败");
         }
 
-
         ICastingGenerator generator = new ConstCastingGenerator();
-        // Stab
-        CastingModel castingModel = new CastingModel(generator);
-        
+        CastingModel castingModel = new CastingModel(generator);        
         CastingResultModel resultModel = castingModel.PredictCastingCurve(null);
+
+        uploadList.remove(uuid);
 
         return resultViewModel;
     }
 
     @GetMapping("/getUploadAndTrainningStatus")
-    public void getUploadAndTrainningStatus()
+    public TrainningStatusViewModel getUploadAndTrainningStatus(@RequestParam(value = "uuid") String uuidString)
     {
+        UUID uuid = UUID.fromString(uuidString);
+        TrainningStatusViewModel statusViewModel = uploadList.get(uuid);
+        if(statusViewModel != null)
+        {
+            // Check status;
+            
+        }
+        else{
+            statusViewModel = new TrainningStatusViewModel();
+            statusViewModel.setStatus("查询不到训练活动的状态。");
+        }
 
+        return statusViewModel;
     }
 }
--- src/system/backend/restservice/src/main/java/xjtuse/castingcurvepredict/viewmodels/TrainningStatusViewModel.java
+++ src/system/backend/restservice/src/main/java/xjtuse/castingcurvepredict/viewmodels/TrainningStatusViewModel.java
@@ -6,4 +6,24 @@ public class TrainningStatusViewModel {
     {
         return _status;
     }
+
+    public void setStatus(String status)
+    {
+        _status = status;
+    }
+
+    private int _statusCode;
+    public int getStatusCode()
+    {
+        return _statusCode;
+    }
+    public void setStatusCode(int statusCode)
+    {
+        _statusCode = statusCode;
+    }
+
+    public double getPercentage()
+    {
+        return 0.5;
+    }
 }
