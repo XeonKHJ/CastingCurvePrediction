4afccaca6e9ce15c12791e547b3f9e5ee00a1836
康宏嘉 <redalertkhj@live.cn>
2021-11-30 21:59:57 +0800

    Save

--- src/system/frontend/src/view.js
+++ src/system/frontend/src/view.js
@@ -2,13 +2,13 @@ var body = document.getElementsByName("body");
 document.createElement()
 
 
-function DisplayError(data)
+function hasError(data)
 {
     var hasError = false;
     var errorMessage;
     if(data.statusCode != undefined && data.status != undefined)
     {
-        if(data.status != 0)
+        if(data.status > 0)
         {
             hasError = true;
         }
@@ -17,3 +17,10 @@ function DisplayError(data)
 
     return hasError;
 }
+
+function displayError(data)
+{
+    document.getElementsByName("body");
+    
+}
+
