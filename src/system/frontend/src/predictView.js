5ca22c4ee1082ea4bacbff4052193617e6fa37de
康宏嘉 <redalertkhj@live.cn>
2021-12-23 13:12:12 +0800

    Save

--- src/system/frontend/src/predictView.js
+++ src/system/frontend/src/predictView.js
@@ -63,22 +63,19 @@ var chartCollectionVueModel = Vue.createApp({
             currentId: 0
         }
     },
-    methods:{
-        revmoveItem(id)
-        {
-            var echartContentDiv = document.getElementById('echartContent'+id);
+    methods: {
+        revmoveItem(id) {
+            var echartContentDiv = document.getElementById('echartContent' + id);
             echartContentDiv.parentNode.removeChild(echartContentDiv);
         },
-        selectItem(id)
-        {
-            var echartContentDiv = document.getElementById('echartContent'+id);
+        selectItem(id) {
+            var echartContentDiv = document.getElementById('echartContent' + id);
             echartContentDiv.style.display = "block"
         }
     }
 }).mount("#chartSection");
 
-function ChartViewModel()
-{
+function ChartViewModel() {
     var defaultDom = document.createElement('div');
     defaultDom.className = "echartContentDiv";
     var defaultChartViewModel = {
@@ -87,8 +84,8 @@ function ChartViewModel()
         data: null,
         isPredictResult: false,
         isSelected: true,
-        echart:null,
-        domeEl:defaultDom,
+        echart: null,
+        domeEl: defaultDom,
     };
 
     return defaultChartViewModel;
@@ -99,10 +96,10 @@ function ChartViewModel()
 function showCastingCurve(data, title, id) {
     var newDataViewModel = ChartViewModel();
     newDataViewModel.chartId = id,
-    newDataViewModel.title = title,
-    newDataViewModel.data = data.castingCurveValues,
-    newDataViewModel.isPredictResult = false,
-    newDataViewModel.isSelected = true
+        newDataViewModel.title = title,
+        newDataViewModel.data = data.castingCurveValues,
+        newDataViewModel.isPredictResult = false,
+        newDataViewModel.isSelected = true
 
     chartCollectionVueModel.chartViewModels.push(newDataViewModel);
     var echartDiv = document.getElementById('echartDiv');
@@ -122,10 +119,8 @@ function showCastingCurve(data, title, id) {
 
 function exportToCsv() {
     var viewModel = null;
-    for(var i = 0; i < chartCollectionVueModel.chartViewModels.length; ++i)
-    {
-        if(chartCollectionVueModel.currentId == chartCollectionVueModel.chartViewModels[i].chartId)
-        {
+    for (var i = 0; i < chartCollectionVueModel.chartViewModels.length; ++i) {
+        if (chartCollectionVueModel.currentId == chartCollectionVueModel.chartViewModels[i].chartId) {
             viewModel = chartCollectionVueModel.chartViewModels[i];
             break;
         }
@@ -134,27 +129,25 @@ function exportToCsv() {
     save(viewModel.title, castingCurveDataToCsv(viewModel.data));
 }
 
-function castingCurveDataToCsv(data)
-{
+function castingCurveDataToCsv(data) {
     var str = "times, values\n";
-    for(var i = 0; i < data.times.length; ++i)
-    {
-        str+=String(data.times[i]) + ", "+ String(data.values[i]) + "\n";
+    for (var i = 0; i < data.times.length; ++i) {
+        str += String(data.times[i]) + ", " + String(data.values[i]) + "\n";
     }
     return str;
 }
 
 function save(filename, data) {
-    const blob = new Blob([data], {type: 'text/csv'});
-    if(window.navigator.msSaveOrOpenBlob) {
+    const blob = new Blob([data], { type: 'text/csv' });
+    if (window.navigator.msSaveOrOpenBlob) {
         window.navigator.msSaveBlob(blob, filename);
     }
-    else{
+    else {
         const elem = window.document.createElement('a');
         elem.href = window.URL.createObjectURL(blob);
-        elem.download = filename;        
+        elem.download = filename;
         document.body.appendChild(elem);
-        elem.click();        
+        elem.click();
         document.body.removeChild(elem);
     }
 }
@@ -184,12 +177,12 @@ function generatingCastingChart(dataViewModel) {
         series: [
             {
                 data: dataViewModel.data.values,
-                yAxis:0,
+                yAxis: 0,
                 type: 'line'
             },
             {
                 data: dataViewModel.data.liqLevel,
-                yAxis:2,
+                yAxis: 2,
                 type: 'line'
             }
         ],
@@ -197,7 +190,7 @@ function generatingCastingChart(dataViewModel) {
         toolbox: {
             show: true,
             feature: {
-                myTool1: { 
+                myTool1: {
                     show: true,//是否显示    
                     title: '导出', //鼠标移动上去显示的文字    
                     icon: 'path://M525.4 721.2H330.9c-9 0-18.5-7.7-18.5-18.1V311c0-9 9.3-18.1 18.5-18.1h336.6c9.3 0 18.5 9.1 18.5 18.1v232.7c0 6 8.8 12.1 15 12.1 6.2 0 15-6 15-12.1V311c0-25.6-25.3-48.9-50.1-48.9h-335c-26.2 0-50.1 23.3-50.1 48.9v389.1c0 36.3 20 51.5 50.1 51.5h197.6c6.2 0 9.3-7.5 9.3-15.1 0-6-6.2-15.3-12.4-15.3zM378.8 580.6c-6.2 0-12.3 8.6-12.3 14.6s6.2 14.6 12.3 14.6h141.4c6.2 0 12.3-5.8 12.3-13.4 0.3-9.5-6.2-15.9-12.3-15.9H378.8z m251.6-91.2c0-6-6.2-14.6-12.3-14.6H375.7c-6.2 0-12.4 8.6-12.4 14.6s6.2 14.6 12.4 14.6h240.8c6.2 0.1 13.9-8.5 13.9-14.6z m-9.2-120.5H378.8c-6.2 0-12.3 8.6-12.3 14.6s6.2 14.6 12.3 14.6h240.8c7.7 0 13.9-8.6 13.9-14.6s-6.2-14.6-12.3-14.6z m119.4 376.6L709 714.1c9.2-12 14.6-27 14.6-43.2 0-39.4-32.1-71.4-71.8-71.4-39.7 0-71.8 32-71.8 71.4s32.1 71.4 71.8 71.4c16.3 0 31.3-5.4 43.4-14.5l31.6 31.5c3.8 3.8 10 3.8 13.8 0 3.8-3.8 3.8-10 0-13.8z m-88.8-23.6c-28.3 0-51.3-22.8-51.3-51s23-51 51.3-51c28.3 0 51.3 22.8 51.3 51s-23 51-51.3 51z', //图标    
@@ -271,36 +264,38 @@ window.onresize = function () {
     resizeEverything();
 }
 
-function onCloseTabButtonClicked() {
+function onCloseTabButtonClicked(el) {
     var i = 0;
     var length = chartCollectionVueModel.chartViewModels.length;
     var echartToDispose;
-    
+
+    idToDelete = el.id.substring(8, el.id.length)
     for (i = 0; i < length; ++i) {
-        if (chartCollectionVueModel.chartViewModels[i].chartId == chartCollectionVueModel.currentId) {
+        if (chartCollectionVueModel.chartViewModels[i].chartId == idToDelete) {
             var element = chartCollectionVueModel.chartViewModels[i];
             echartToDispose = element;
 
             if (length == 1) {
-                chartCollectionVueModel.chartViewModels.push(defaultChartViewModel);
+                chartCollectionVueModel.chartViewModels.push(ChartViewModel());
+                selectItem(0);
                 chartCollectionVueModel.isEmpty = true;
             }
-            else if (length == i + 1) {
+            else if(chartCollectionVueModel.currentId == idToDelete) {
+
+                if (length == i + 1) {
                     selectItem(chartCollectionVueModel.chartViewModels[i - 1].chartId);
-            }
-            else {
-                    var chartId = chartCollectionVueModel.chartViewModels[i+1].chartId;
-                    var el = document.getElementById('echartContent'+ chartId);
+                }
+                else {
+                    var chartId = chartCollectionVueModel.chartViewModels[i + 1].chartId;
+                    var el = document.getElementById('echartContent' + chartId);
                     selectItem(chartCollectionVueModel.chartViewModels[i + 1].chartId);
+                }
             }
 
-            
-            if (element.echart != null) {
-                element.echart.clear();
-                element.echart.dispose();
+            if (echartToDispose.echart != null) {
+                echartToDispose.echart.clear();
+                echartToDispose.echart.dispose();
             }
-
-
 
             chartCollectionVueModel.chartViewModels.splice(i, 1);
             break;
@@ -346,8 +341,7 @@ function selectItem(id) {
     });
 }
 
-function getChartViewModelById(id)
-{
+function getChartViewModelById(id) {
     var requestViewModel = null;
     chartCollectionVueModel.chartViewModels.forEach(element => {
         if (element.chartId == id) {
@@ -358,17 +352,14 @@ function getChartViewModelById(id)
     return requestViewModel;
 }
 
-function onPlayButtonClicked()
-{
+function onPlayButtonClicked() {
     currentViewModel = getChartViewModelById(chartCollectionVueModel.currentId);
-    if(currentViewModel.data != null)
-    {
+    if (currentViewModel.data != null) {
         playAnimation(currentViewModel.data);
     }
 }
 
-function playAnimation(data)
-{
+function playAnimation(data) {
     translateData = data;
     startTime = Date.now();
 }
