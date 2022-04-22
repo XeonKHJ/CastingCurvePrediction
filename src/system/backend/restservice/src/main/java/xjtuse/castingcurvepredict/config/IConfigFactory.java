016deaeb097f841a72d5ca07687539bb2e7f4839
康宏嘉 <redalertkhj@live.cn>
2022-04-23 05:50:36 +0800

    save.

--- src/system/backend/restservice/src/main/java/xjtuse/castingcurvepredict/config/IConfigFactory.java
+++ src/system/backend/restservice/src/main/java/xjtuse/castingcurvepredict/config/IConfigFactory.java
@@ -3,9 +3,11 @@ package xjtuse.castingcurvepredict.config;
 import xjtuse.castingcurvepredict.castingpredictiors.ICastingGenerator;
 import xjtuse.castingcurvepredict.castingpredictiors.IStatusManager;
 import xjtuse.castingcurvepredict.models.TaskModel;
+import xjtuse.castingcurvepredict.viewmodels.IViewModel;
 
 public interface IConfigFactory {
     ICastingGenerator getCastingGenerator();
     String getModelDir();
     IStatusManager getStatusManager(TaskModel model);
+    void setErrorMessageOnViewModel(IViewModel viewModel, Exception exception);
 }
