bc0389706c3185af9966bca886d4024529c5b30a
康宏嘉 <redalertkhj@live.cn>
2022-05-24 22:32:26 +0800

    添加采样率的修改。

--- src/approaches/docgaomethod/gaomethod.py
+++ src/approaches/docgaomethod/gaomethod.py
@@ -13,7 +13,8 @@ h = 1 # 塞棒高度
 
 
 def stp_pos_flow(h, lv_act, t):
-    H1 = 651+(42/19)*(t)  # H1中间包液位高度，时间函数，时间步为半秒，要除2
+    H1t = 651+(42/19)*(t)  # H1中间包液位高度，时间函数，时间步为半秒，要除2
+
     
     # 引锭头顶部距离结晶器底部高度350+结晶器液位高度（距离引锭头）283
     if lv_act < 633:
@@ -21,21 +22,22 @@ def stp_pos_flow(h, lv_act, t):
     else:
         H3 = lv_act-633  # H3下侧出口淹没高度
     c2 = 0.2184*(h+15)-2.0283  # c值，action是-15至15，先加15
-    dL = (pow(20, 0.5) * c2*A*pow((H1+H2-H3), 0.5)*0.5) / (B * W)
+    dL = (pow(20, 0.5) * c2*A*pow((H1t+H2-H3), 0.5)*0.5) / (B * W)
     return dL
-
 
 if __name__ == '__main__':
     hs = [100, 200, 300, 400, 500]
-    t = [10, 20, 10, 20, 10]
+    ts = [10, 20, 10, 20, 10]
+    sampleRate = 2 # 采样率是2Hz。
     lv_acts = list()
     lv_act = 0
     for stage in range(5):
-        sampleCount = t[stage]
-        for time in range(sampleCount):
+        stopTimeSpan = ts[stage]
+        for time in range(stopTimeSpan):
             if stage > 0:
-                previousTime = t[stage-1]
+                previousTime = ts[stage-1]
             else:
                 previousTime = 0
-            lv_act += stp_pos_flow(hs[stage], lv_act, previousTime + time)
-            lv_acts.append(lv_act)
+            for sampleCount in range(sampleRate):
+                lv_act += stp_pos_flow(hs[stage], lv_act, previousTime + time + (sampleCount / sampleRate))
+                lv_acts.append(lv_act)
